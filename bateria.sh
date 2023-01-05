8cec74e5a055f2d1ac31e247dfd38baa8036ed97
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2023-01-05 16:29:03 -0300

    Create bateria.sh
    Script feito para treino e conseguir ler a bateria do notebook via ssh

---
+++ bateria.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Script para trazer status da bateria do notebook.
+#
+#Desenvolvido por DevChimpa Dia: 05/01/2023
+#E-mail para contato: chimpadeveloper@gmail.com
+#
+
+BATERIA=$(cat /sys/class/power_supply/BAT0/uevent | grep POWER_SUPPLY_CAPACITY= | cut -c 23-25)
+STATUS_BATERIA=$(cat /sys/class/power_supply/BAT0/uevent | grep POWER_SUPPLY_STATUS | tr = ' ' | cut -d' ' -f2)
+
+clear
+
+if [ $BATERIA -lt 20 ]
+then
+        echo " Bateria em: $BATERIA Status: Bateria Baixa"
+        echo " POR FAVOR CONECTE A UMA FONTE DE ENERGIA "
+
+elif  [ $STATUS_BATERIA = "Full" ] && [ $BATERIA -eq 100 ]
+then
+        echo " Bateria em: $BATERIA% Status: Carregada."
+elif [ $STATUS_BATERIA = "Discharging" ]
+then
+        echo " Bateria: em $BATERIA% Status: Descarregando."
+else
+        echo " $BATERIA% Status: Carregando."
+fi
+#echo "Bateria em: $BATERIA% STATUS: $STATUS_BATERIA"
